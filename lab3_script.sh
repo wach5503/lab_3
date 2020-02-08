4fa7eb8b3dfb9b68b0a2fef77608d2892524869a
wach5503 <wach5503@colorado.edu>
2020-02-07 17:16:45 -0700

    Adding all of the files for lab 3

--- lab3_script.sh
+++ lab3_script.sh
@@ -3,5 +3,29 @@
 # Date: 2/7/2020
 
 #Problem 1 Code:
-	
-#Make sure to document how you are solving each problem!
+vim file.sh
+//write something
+i
+echo "Chen"
+echo "^The"
+::wq
+
+#p2 code:
+grep "Chen" file.sh 
+
+#p3 code:
+egrep -c "[0-9][0-9][0-9]-[0-9][0-9][0-9]-[0-9][0-9][0-9][0-9]" regex_practice.txt
+
+
+#p4 code:
+#1
+grep -c  "@" regex_practice.txt
+#2
+grep "303" regex_practice.txt
+#3
+grep "@geocities.com" regex_practice.txt >> email_results.txt
+
+#p5&6  code:
+git add email_results.txt
+git commit -m "done"
+git push
